f25f2135afd694c8253407c2a1f245e838b7f6cb
Tetra Quark <56183794+quarktetra@users.noreply.github.com>
2024-10-08 23:49:20 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,5 @@
-import React from 'react';
-
-import '../styles/globals.scss';
-
+import React from 'react'; 
+import '../styles/globals.scss'; 
 function MyApp({ Component, pageProps }) {
-  // Avoid recursive calls here
-  return <Component {...pageProps} />
-}
-
+return <Component {...pageProps} /> } 
 export default MyApp;
